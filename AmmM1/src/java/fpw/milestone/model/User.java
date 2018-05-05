970a9c2db899e64487ebd189a242af0df9d4695f
marty-p <marty-p@outlook.com>
2018-05-05 16:46:29 +0200

    +user refactory

--- AmmM1/src/java/fpw/milestone/model/User.java
+++ AmmM1/src/java/fpw/milestone/model/User.java
@@ -11,73 +11,95 @@ package fpw.milestone.model;
  */
 public class User {
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * @return the surname
-     */
-    public String getSurname() {
-        return surname;
-    }
+	/**
+	 * @return the surname
+	 */
+	public String getSurname() {
+		return surname;
+	}
 
-    /**
-     * @param surname the surname to set
-     */
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	/**
+	 * @param surname the surname to set
+	 */
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    /**
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
-    }
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
 
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    /**
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
 
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setId(int id){
-        this.id = id;
-    }
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
 
-    public int getId(){
-        return this.id;
-    }
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
 
+	/**
+	 * @return the category
+	 */
+	public Category getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	private enum Category { AUTHOR, READER, GUEST };
     private String name;
     private String surname;
     private String username;
     private String password;
     private int id;
+    private Category category;
 }
